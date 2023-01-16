0dd179e12b7887a9a7d94b5a345a1c1698d00f95
AlanRacciatti <alanracciatti1220@gmail.com>
2023-01-16 09:16:38 -0300

    feat: add exploit validation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,6 @@ import "@nomicfoundation/hardhat-toolbox";
 require("dotenv").config();
 
 if (!process.env.PRIVATE_KEY) process.exit(1);
-if (!process.env.ETHERSCAN_API_KEY) process.exit(1);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -12,11 +11,6 @@ const config: HardhatUserConfig = {
       chainId: 5,
       url: process.env.GOERLI_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    apiKey: {
-      goerli: process.env.ETHERSCAN_API_KEY,
     },
   },
 };
